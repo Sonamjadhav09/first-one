9470b289169770d98a0ea5602854579892e4b1a9
Sonamjadhav09 <sonamjadhavjadhva@gmail.com>
2024-07-12 17:41:06 +0530

    first-one

---
+++ Lecture15.js/new.js
@@ -0,0 +1,35 @@
+//console.log('hello')
+// Number
+// String
+// Boolean
+// BigInt
+// Symbol
+// null
+//undefined
+var a=5
+console.log(a);
+let b=10 ;
+console.log(b);
+ const c=15;
+ console.log(c);
+ //camel case
+ //let firstName
+ let firstName='mintu';
+ let lastName='sharma';
+ //console.log(firstName+" " +lastName);
+
+
+//bagtick=`
+//console.log(` my firstname is ${firstName} and lastname is ${lastName}`);
+console.log('mintu\nsharma');
+//= vs == vs ===
+let score=100;
+let to =score==100;
+console.log(to);
+console.log(false==='');
+console.log(false==='false');
+
+
+
+
+
